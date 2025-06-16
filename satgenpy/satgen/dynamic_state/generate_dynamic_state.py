1ef513dfaafc9b613df2e04b6c8eba1b781f0a10
Kun <kun.cs12@nycu.edu.tw>
2025-06-16 14:41:39 +0800

    modify: satgenpy/satgen/dyna_state/gener_dynam_state.py

--- satgenpy/satgen/dynamic_state/generate_dynamic_state.py
+++ satgenpy/satgen/dynamic_state/generate_dynamic_state.py
@@ -29,6 +29,7 @@ from .algorithm_free_one_only_gs_relays import algorithm_free_one_only_gs_relays
 from .algorithm_free_one_only_over_isls import algorithm_free_one_only_over_isls
 from .algorithm_paired_many_only_over_isls import algorithm_paired_many_only_over_isls
 from .algorithm_free_gs_one_sat_many_only_over_isls import algorithm_free_gs_one_sat_many_only_over_isls
+from .algorithm_hierarchical import algorithm_hierarchical
 
 
 def generate_dynamic_state(
@@ -281,6 +282,22 @@ def generate_dynamic_state_at(
             list_gsl_interfaces_info,
             prev_output,
             enable_verbose_logs
+        )
+
+    elif dynamic_state_algorithm == "algorithm_hierarchical":
+
+        return algorithm_hierarchical(
+            output_dynamic_state_dir,
+            time_since_epoch_ns,
+            satellites,
+            ground_stations,
+            sat_net_graph_only_satellites_with_isls,
+            ground_station_satellites_in_range,
+            num_isls_per_sat,
+            sat_neighbor_to_if,
+            list_gsl_interfaces_info,
+            prev_output,
+            enable_verbose_logs,
         )
 
     else:
