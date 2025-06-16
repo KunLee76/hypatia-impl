6e98eb03001ca4166a2c07cdd88def1b2d605db9
Kun <kun.cs12@nycu.edu.tw>
2025-06-16 12:45:18 +0800

    modify: integ. test_run_list-chosen pair

--- integration_tests/test_manila_dalian_over_kuiper/run_list.py
+++ integration_tests/test_manila_dalian_over_kuiper/run_list.py
@@ -35,9 +35,11 @@ dynamic_state = "dynamic_state_" + str(dynamic_state_update_interval_ms) + "ms_f
 # > Manila (17) to Dalian (18)
 full_satellite_network_isls = "reduced_kuiper_630_algorithm_free_one_only_over_isls"
 full_satellite_network_isls_sat_many = "reduced_kuiper_630_algorithm_free_gs_one_sat_many_only_over_isls"
+full_satellite_network_hierarchical = "reduced_kuiper_630_algorithm_hierarchical"
 chosen_pairs = [
     ("kuiper_630_isls_sat_one", 17, 18, "TcpNewReno", full_satellite_network_isls),
     ("kuiper_630_isls_sat_many", 17, 18, "TcpNewReno", full_satellite_network_isls_sat_many),
+    ("kuiper_630_hierarchical", 17, 18, "TcpNewReno", full_satellite_network_hierarchical),
 ]
 
 
